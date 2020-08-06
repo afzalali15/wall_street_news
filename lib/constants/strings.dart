e1918b03fa995695b44b2541dc42fe12da09ca94
Afzal Ali <afzalali15@gmail.com>
2020-08-06 12:52:07 +0400

    Update strings.dart

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -1,4 +1,4 @@
 class Strings {
   static String news_url =
-      'http://newsapi.org/v2/everything?domains=wsj.com&apiKey=8b0cf2b33ae94b67bce1f1fb602aff4c';
+      'http://newsapi.org/v2/everything?domains=wsj.com&apiKey=API_KEY';
 }
